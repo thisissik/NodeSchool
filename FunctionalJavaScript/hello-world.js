48d5c5b3248fde6e94a60fe4e8eb1fe39c1e39be
Sung Sik Kim <thisissik15@gmail.com>
2018-02-27 16:25:42 -0500

    Start Functional-Javascript

---
+++ FunctionalJavaScript/hello-world.js
@@ -0,0 +1,5 @@
+function toUpperCase(text) {
+    return text.toUpperCase();
+}
+
+module.exports = toUpperCase;
---
+++ FunctionalJavaScript/repeat.js
@@ -0,0 +1,6 @@
+module.exports = function(operation, num) {
+    while (num > 0) {
+        operation();
+        num--;
+    }
+}
